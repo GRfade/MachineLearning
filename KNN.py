5e42b55621018bddfbc11d4aa8a85d6fd353499d
郭锐 <1095461432@qq.com>
2020-11-02 19:17:00 +0800

    完成数据输入函数，计算欧式距离函数

--- KNN.py
+++ KNN.py
@@ -9,40 +9,61 @@ from scipy.io import arff #方便导入arff文件数据
 
 
 '''
-本算法为KNN算法。KNearestNeighbor
+本算法为KNN算法。KNearestNeighbor 监督学习当中的分类算法
 可采用归一化数据方式，将所有数据范围压缩到0~1
+采用欧式距离计算
 本次算法调用数据：diabetes.arff 糖尿病人的各项数据
 数据内容包括：preg（怀孕次数）、plas（葡萄糖浓度）、pres（血压）、skin（皮肤厚度）、insu（胰岛素）、mass（体重）、 pedi（谱系功能）、 age（年龄）、 class(是否为糖尿病人)
-前8列为特征、最后一列为
+前8列为特征、最后一列为是否患有糖尿病
 '''
 
 #读入数据
 def readingDatas():
-    data = arff.loadarff("./Dataset/diabetes.arff")
-    df = pd.DataFrame(data[0])
-    # print(data)
-    print(df)
-    print(type(df))
+    #从diabetes.arff文件中导入数据，最终转化为两个，一个列表为特征值，一个列表为分类结果
+    array = arff.loadarff("./Dataset/diabetes.arff")
+    df = pd.DataFrame(array[0])
+
+    data_array = np.array(df)
+    data_list = data_array.tolist()
+    data = []
+    results = []
+    for list in data_list:
+        data.append(list[:-1])
+        results.append(list[-1])
+    return data,results
+
+#计算两个数据之间的欧式距离
+def distance(datalist,target):
+    dist = []
+    vec1 = np.array(target)
+    for data in range(0,len((datalist))):
+        vec2 = np.array(data)
+        dist.append(np.linalg.norm(vec1-vec2))
+    return dist
 
 
-
-##不需要归一化
 def transfun(data):
     '''将data归一化，'''
     sum = np.sum(np.std(data, axis=0))
     mean = data.mean(axis=0)
     data = (data -mean) / sum
     return data
+
+
 #定义KNN分类器函数
 #函数参数包括：（测试数据，训练数据，分类，k值）
-def KNNclassify(inX,dataSet, labels, k):
+def KNNclassify(dataset, labels, k):
     print("classify")
 
 
+
 def testKNN():
-    print("test")
+    print("testKNN")
+
 
 if __name__ == "__main__":
     testKNN()
-    readingDatas()
+    data,results = readingDatas()
+    # dist = distance(data,[0,1,2,3,4,5,6,7])
+    # print(len(dist))
 
