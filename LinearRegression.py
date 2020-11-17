c302f9c1d428e7f58502692408bb1dab6259dad3
郭锐 <1095461432@qq.com>
2020-11-17 19:56:58 +0800

    线性回归函数完成训练集

--- LinearRegression.py
+++ LinearRegression.py
@@ -158,16 +158,7 @@ def RSequre(testData,coefficient):
     return result
 
 
-def function():
-    sns.set(style="ticks")
 
-    # Load the example dataset for Anscombe's quartet
-    df = sns.load_dataset("anscombe")
-
-    # Show the results of a linear regression within each dataset
-    sns.lmplot(x="x", y="y", col="dataset", hue="dataset", data=df,
-               col_wrap=2, ci=None, palette="muted", height=4,
-               scatter_kws={"s": 50, "alpha": 1})
 
 def testLinear():
     '''
