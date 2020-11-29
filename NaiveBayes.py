cbdec316bdeb658145a98b518edffee6eed33e3b
郭锐 <1095461432@qq.com>
2020-11-29 18:35:30 +0800

    使用拉普拉斯平滑

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,11 +1,11 @@
-import math  #数学基本运算
-import matplotlib.pyplot as plt #图形显示
-import random  #随机数
+# import math  #数学基本运算
+# import matplotlib.pyplot as plt #图形显示
+# import random  #随机数
 import numpy as np #矩阵运算库
 import pandas as pd #提供高性能易用数据类型和分析工具
-import seaborn as sns #绘制数据分布，数据观察函数
+# import seaborn as sns #绘制数据分布，数据观察函数
 from scipy.io import arff #方便导入arff文件数据
-import sys #用于表示最大值和最小值
+# import sys #用于表示最大值和最小值
 
 
 
---
+++ SoftmaxRegression.py
@@ -0,0 +1,26 @@
+import math  #数学基本运算
+import matplotlib.pyplot as plt #图形显示
+import random  #随机数
+import numpy as np #矩阵运算库
+import pandas as pd #提供高性能易用数据类型和分析工具
+import seaborn as sns #绘制数据分布，数据观察函数
+from scipy.io import arff #方便导入arff文件数据
+import sys #用于表示最大值和最小值
+
+
+
+'''
+该算法为softmaxRegression算法 
+数据来源：iris.arff 鸢尾属植物的各项数据
+数据内容包括：
+
+'''
+
+
+
+
+def testDemo():
+    print('test')
+
+if __name__ == "__main__":
+    testDemo()
